6460665a211832fecafa3abb9c120f3c9c87f9ca
lukapelc <lukapelc91@gmail.com>
2017-06-02 14:24:00 +0200

    test na upload2

--- upload.php
+++ upload.php
@@ -35,10 +35,10 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.". $target_file;
+        echo "Sorry, there was an error uploading your file."." target file:". $target_file ."iploadok: ".$uploadOk."target dir". $target_dir;
     }
 }
 ?>
